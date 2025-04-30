20b02b91ac64e212e2743f343afe0f84272a1524
dbryn24 <davebryangamer@gmail.com>
2025-04-30 21:52:03 +0800

    structure update

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const usersController = require("./app/user/user.controller");
+const usersController = require("./app/user/inventory.controller");
+const inventoryController = require("./app/inventory/inventory.controller");
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", usersController);
+app.use("/api", inventoryController);
 
 module.exports = app;
